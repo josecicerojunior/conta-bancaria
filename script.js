34ff10d178f8e9546c32274c6d90d3ce1208caf7
grajeiro123 <juniorgranjeiro.jc@gmail.com>
2025-09-05 11:34:57 -0300

    método depositar implementado

--- script.js
+++ script.js
@@ -25,8 +25,16 @@ class Conta {
 
 const conta1 = new Conta(101, "José", 100);
 const conta2 = new Conta(102, "Maria", 200);
+const conta3 = new Conta(103, "Antonio", 100);
 
 // console.log(conta1)
 
 conta1.consultarSaldo();
 conta2.consultarSaldo();
+
+conta1.depositar(200);
+conta1.consultarSaldo();
+conta2.depositar(500);
+conta2.consultarSaldo();
+conta3.depositar(350);
+conta3.consultarSaldo();
